1805cb9649a75085cbabf5718cc4ae6efcba4c1b
Robert Hook <rahook@gmail.com>
2020-08-13 16:07:17 +0100

    move terraform state to group account

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,11 +1,11 @@
 terraform {
-  backend s3 {
-    bucket         = "lbatrain20200428162949619600000001"
-    key            = "wasabi"
+  backend "s3" {
+    bucket         = "lbagroup20191214122212241800000001"
+    key            = "wasabi_presentation"
     region         = "eu-west-2"
     encrypt        = true
-    kms_key_id     = "arn:aws:kms:eu-west-2:422515236307:key/dbb95ea0-24e8-41dc-9ce8-279741fac2ad"
+    kms_key_id     = "arn:aws:kms:eu-west-2:538526694679:key/dd0c2139-bc2a-43db-9f22-b94a033c7766"
     dynamodb_table = "terraform-state-lock"
-    profile        = "lba_train"
+    profile        = "lba_group"
   }
 }
